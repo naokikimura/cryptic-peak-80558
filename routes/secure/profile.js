85486344efb34a5494e8de2dd9c67ad02e23ed83
naokikimura <n.kimura.cap@gmail.com>
2018-11-23 17:49:12 +0900

    Update app.js

---
+++ routes/secure/profile.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var profile = {};
+
+router.get('/edit', (req, res, next) => {
+  res.render('secure/profile/edit', { profile: profile });
+});
+
+router.post('/preview', (req, res, next) => {
+  const profile = {
+    name: req.body.name,
+    email: req.body.email,
+    introduction: req.body.introduction
+  };
+  req.session.profile = profile;
+  res.setHeader('X-XSS-Protection', 0);
+  res.render('secure/profile/preview', { profile: profile });
+});
+
+router.post('/update', (req, res, next) => {
+  profile = req.session.profile || {};
+  res.redirect('edit');
+});
+
+module.exports = router;
